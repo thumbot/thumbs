c3369ba037ca3ca2558891f5416d9a4d1f6a02f3
David Andersen <davidx@gmail.com>
2016-08-08 10:24:09 -0700

    move test deps

--- test/test.rb
+++ test/test.rb
@@ -1,10 +1,8 @@
 ENV['RACK_ENV'] = 'test'
 
-$:.unshift(File.join(File.dirname(__FILE__), '/../'))
-require 'app'
-require 'test/unit'
-require 'rack/test'
-require 'dust'
+$:.unshift(File.dirname(__FILE__))
+
+require 'test_helper'
 require 'test/test_integrations'
 require 'test/test_webhook'
 require 'test/test_slack'
